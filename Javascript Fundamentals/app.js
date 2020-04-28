0a1ea5ef4abe468b1634d4891d503d0dfeb9bb2c
dchoi-tech <luckychoido@gmail.com>
2020-04-28 12:04:04 -0700

    Javascript Fundamental
    lesson first - using console

---
+++ Javascript Fundamentals/app.js
@@ -0,0 +1,31 @@
+// log to console
+console.log("Hello world");
+console.log(123);
+console.log(true);
+var greeting = "hello";
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error("this is some error");
+console.clear();
+console.warn("this is warning");
+console.time("hello");
+console.log("Hello world");
+console.log(123);
+console.log("Hello world");
+console.log(123);
+console.log("Hello world");
+console.log(123);
+console.log("Hello world");
+console.log(123);
+console.log("Hello world");
+console.log(123);
+console.log("Hello world");
+console.log(123);
+console.timeEnd("hello");
+/* 
+multi
+line
+comments
+ */
